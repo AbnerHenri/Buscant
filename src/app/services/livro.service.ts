881493faa43119a23ad12997f0375c15dc528d90
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-07-25 17:28:49 -0300

    Create Models and Modified List.ts

--- src/app/services/livro.service.ts
+++ src/app/services/livro.service.ts
@@ -1,18 +1,23 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+
+import { Item, LivroResultado } from '../models/intefaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LivroService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private  http: HttpClient) { }
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes?';
 
-  private readonly API = 'https://www.googleapis.com/books/v1/volumes?'
-
-  find(enteredValue : string): Observable<any> {
-    const params = new HttpParams().append('q', enteredValue)
-    return this.http.get(this.API, { params })
+  find(enteredValue: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', enteredValue);
+    return this.http.get<LivroResultado>(this.API, { params }).pipe(
+      tap((resAPI) => console.log('Primeiro Fluxo', resAPI)),
+      map((resMap) => resMap.items),
+      tap((resTap) => console.log('Fluxo após Map', resTap))
+    );
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -4,21 +4,15 @@ import { LivroService } from 'src/app/services/livro.service';
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
-
   listaLivros: [];
-  campoBusca: string = ''
+  campoBusca: string = '';
 
-  constructor(private servive: LivroService) { }
+  constructor(private servive: LivroService) {}
 
   findBooks() {
-    this.servive.find(this.campoBusca).subscribe((res) => {
-      console.log(res)
-    })
+    this.servive.find(this.campoBusca).subscribe();
   }
 }
-
-
-
