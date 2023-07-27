fb916af26878db1ef96fa4f5c5de20195eb5c605
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-07-27 08:20:18 -0300

    Add Filter Pipe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, switchMap, tap } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs';
 import { Item, Livro } from 'src/app/models/intefaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/services/livro.service';
@@ -16,6 +16,7 @@ export class ListaLivrosComponent {
   constructor(private service: LivroService) {}
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    filter((valorDigitado) => valorDigitado.length >= 3),
     switchMap((valorDigitado) => this.service.find(valorDigitado)),
     map((items) => this.resultBooks(items)),
     tap((res) => console.log(res))
